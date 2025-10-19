a2de46bf6944407cf367b5bc4b06b1ead01e10d2
Jonas Nywall <jonas@nywall.org>
2025-10-19 17:53:59 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,49 @@
+radio.setTransmitPower(3)
+radio.setGroup(1)
+let eyeState = 0
+basic.forever(function () {
+    radio.sendNumber(eyeState)
+    if (eyeState == 0) {
+        basic.showLeds(`
+            . . . . .
+            # # # . .
+            # # # # .
+            # # # # .
+            # # # # .
+            `)
+    } else if (eyeState == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . # # #
+            . # # # #
+            . # # # #
+            . # # # #
+            `)
+    } else if (eyeState == 2) {
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    } else if (eyeState == 3) {
+        basic.showLeds(`
+            . . . . .
+            # # . . .
+            # # # # .
+            . . # # #
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    basic.pause(randint(2000, 6000))
+    eyeState = randint(0, 4)
+})
